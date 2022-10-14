6a739b5cf8fb52b4f84f20c84b60a5ca794e6b4f
GitHub Action <action@github.com>
2022-10-14 19:38:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/nilsa/intellijsarif/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nilsa/intellijsarif/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nilsa.intellijsarif.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nilsa.intellijsarif.MyBundle
 
 class MyApplicationService {
 
